00b52f4915b884234bba0067a80c10d42d3e1bb6
Sumer Mixon <sumer@ximm.us>
2017-08-18 10:33:14 -0400

    keeping track of answers

--- quizzer/scripts.js
+++ quizzer/scripts.js
@@ -1,3 +1,52 @@
-window.onload = function() {
-		console.log("It Works!");
-};
+// empty array to hold the answers
+var answers = [];
+
+function validateSelection() {
+	// get the radio form
+	var selection = document.answers.selection;
+
+	// Get the submit button
+	var submit = document.getElementById("submit");
+
+	// Check if there is a selection 
+	for (var i=0; i<selection.length; i++) {
+    	if (selection[i].checked){
+        	submit.disabled = false;
+        	break;
+    	}
+     }
+     if (i==selection.length){
+     	submit.disabled = true;
+     }
+ }
+
+// helper function to get answers
+function getAnswer() {
+    var question = document.getElementById("question");
+    var answer = [];
+    var i;
+    for (i = 0; i < question.length; i++) {
+        if (question[i].checked) {
+            // answer = answer + question.name;
+            answer.push(question[i].value);
+            question[i].checked = false; // clear selection on submit
+            validateSelection();
+        }
+    }
+    // Add answer to answers array
+    answers.push(answer);
+}
+
+function getAnswers() {
+	for (i = 0; i < answers.length; i++) {
+		var question = i+1;
+		console.log("Answer for " + question + " was: " + answers[i][0]);
+	}
+}
+
+   ///////////
+  // TO DO //
+ //////////
+/**
+1. Dynamic content loading
+**/
