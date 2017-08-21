3c44084d52e4eec28388051c9823437b32b850b8
Sumer Mixon <sumer@ximm.us>
2017-08-21 15:54:03 -0400

    Adding new component: Login Modal

---
+++ login modal/script.js
@@ -0,0 +1,84 @@
+  /////////////////
+ // Login modal //
+/////////////////
+
+var modal = document.getElementById('login-modal');
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// Open modal on load 
+window.onload = openModal();
+
+  //////////////////////
+ // Login validation //
+//////////////////////
+
+// Dummy creditials
+var credintials = {
+	email: "sumer@ximm.us",
+	password: "password",
+	remember: false
+};
+
+// Get submit button
+var submit = document.login.submit;
+
+// get email and password
+var email = document.login.email.value;
+var password = document.login.password.value;
+
+function validateInput() {
+	// update email and password
+	email = document.login.email.value;
+	password = document.login.password.value;
+	
+
+	// Check if email or password is blank, if not enable submit button 
+   	if ((( email != "" ) && ( password != "" ) && ( password.length>=6 ))){
+   		submit.disabled = false;
+   	} else { submit.disabled = true; }
+}
+
+function validateEmail() {
+	// Update email and error message	
+	email = document.login.email.value;
+	emailError = document.getElementById('email-error');
+
+	// Validate email 
+	var atpos = email.indexOf("@");
+    var dotpos = email.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+        emailError.style.display = "inline";
+        return false;
+    } else emailError.style.display = "none";
+}
+
+function validateCredintials() {
+	email = document.login.email.value;
+	password = document.login.password.value;
+	remember = document.login.remember.checked; // Checked? true of false
+	
+	var loginError = document.getElementById('login-error');
+	var loginForm = document.getElementById("userLogin");
+
+	if (( email == credintials.email ) && ( password == credintials.password )) {
+		console.log("Success" + " " + remember );
+		closeModal();
+	} else {
+		console.log("Nope");
+		loginError.style.display = "inline";
+		loginForm.reset();
+		submit.disabled = true;
+	}
+}
+
+// Listen to form input and fire validation
+window.addEventListener('input', function (e) {
+	validateInput();
+}, false);
