0f82c1cd428c2ec1924db1f483ae21e4de929bc6
Sumer Mixon <sumer@ximm.us>
2017-09-06 15:57:23 -0400

    long hold function done

--- long hold/scripts.js
+++ long hold/scripts.js
@@ -1,3 +1,24 @@
-function longHold(){
-	console.log("fire!");
+function longHold(theClicked){
+
+	var theClicked = document.getElementById(theClicked);
+	var hiddenText = document.getElementById("hidden-text");
+
+	function fireIt(){
+		hiddenText.classList.toggle('hidden');
+	}
+
+	var theTimer;
+	
+	function startTimer(){
+		theTimer = setTimeout(fireIt, 2000);
+	}
+
+	function stopTimer(){
+		clearTimeout(theTimer);
+	}
+	
+	theClicked.addEventListener("mousedown", startTimer);
+	theClicked.addEventListener("mouseup", stopTimer);
 }
+
+longHold("long-click");
